b86f8b387b2ebaec7b4c6d0e41b8c0ff258449f4
unknown <hjimmy0803@gmail.com>
2020-02-26 00:01:38 -0600

    Made two of my players role random dice.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
+var randNum1 = Math.floor((Math.random() * 6) + 1);
+var playerOneDice1 = 'Images/dice' + randNum1 + '.png';
+var randNum2= Math.floor((Math.random() * 6) + 1);
+var playerTwoDice2 = 'Images/dice' + randNum2 + '.png';
+
+
+document.querySelector('.img1').setAttribute('src',playerOneDice1);
+document.querySelector('.img2').setAttribute('src',playerTwoDice2);
+
 
 
 
