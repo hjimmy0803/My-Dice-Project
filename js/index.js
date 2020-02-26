40f1cc9b78e1db5ef25f65a32a7c50d8229cede6
unknown <hjimmy0803@gmail.com>
2020-02-26 07:33:38 -0600

    Made all players able to role a random number each time the page refreshes.

--- js/index.js
+++ js/index.js
@@ -2,10 +2,34 @@ var randNum1 = Math.floor((Math.random() * 6) + 1);
 var playerOneDice1 = 'Images/dice' + randNum1 + '.png';
 var randNum2= Math.floor((Math.random() * 6) + 1);
 var playerTwoDice2 = 'Images/dice' + randNum2 + '.png';
+var randNum3= Math.floor((Math.random() * 6) + 1);
+var playerTwoDice3 = 'Images/dice' + randNum3 + '.png';
+var randNum4= Math.floor((Math.random() * 6) + 1);
+var playerTwoDice4 = 'Images/dice' + randNum4 + '.png';
+var randNum5= Math.floor((Math.random() * 6) + 1);
+var playerTwoDice5 = 'Images/dice' + randNum5 + '.png';
+var randNum6= Math.floor((Math.random() * 6) + 1);
+var playerTwoDice6 = 'Images/dice' + randNum6 + '.png';
+var randNum7= Math.floor((Math.random() * 6) + 1);
+var playerTwoDice7 = 'Images/dice' + randNum7 + '.png';
+var randNum8= Math.floor((Math.random() * 6) + 1);
+var playerTwoDice8 = 'Images/dice' + randNum8 + '.png';
+var randNum9= Math.floor((Math.random() * 6) + 1);
+var playerTwoDice9 = 'Images/dice' + randNum9 + '.png';
+var randNum10= Math.floor((Math.random() * 6) + 1);
+var playerTwoDice10 = 'Images/dice' + randNum10 + '.png';
 
 
 document.querySelector('.img1').setAttribute('src',playerOneDice1);
 document.querySelector('.img2').setAttribute('src',playerTwoDice2);
+document.querySelector('.img3').setAttribute('src',playerTwoDice3)
+document.querySelector('.img4').setAttribute('src',playerTwoDice4)
+document.querySelector('.img5').setAttribute('src',playerTwoDice5)
+document.querySelector('.img6').setAttribute('src',playerTwoDice6)
+document.querySelector('.img7').setAttribute('src',playerTwoDice7)
+document.querySelector('.img8').setAttribute('src',playerTwoDice8)
+document.querySelector('.img9').setAttribute('src',playerTwoDice9)
+document.querySelector('.img10').setAttribute('src',playerTwoDice10)
 
 
 
