f8f2bb1690324fe8e7cec42fdcf5778366ec8f43
unknown <hjimmy0803@gmail.com>
2020-02-27 07:36:01 -0600

    Changed my whole code.

--- js/index.js
+++ js/index.js
@@ -1,60 +1,37 @@
-// var randNum1 = Math.floor((Math.random()*4)+1);
-// var randNum2 = Math.floor((Math.random()*6)+1);
-// var randNum3 = MAth.floor((Math.random()*8)+1);
-// var randNum4 = Math.floor((Math.random()*10)+1);
-// var randNum5 = Math.floor((Math.random()*12)+1);
-// var randNum6 = Math.floor((Math.random()*20)+1);
+var randNum1 = Math.floor((Math.random()*4)+1);
+var randNum2 = Math.floor((Math.random()*6)+1);
+var randNum3 = Math.floor((Math.random()*8)+1);
+var randNum4 = Math.floor((Math.random()*10)+1);
+var randNum5 = Math.floor((Math.random()*12)+1);
+var randNum6 = Math.floor((Math.random()*20)+1);
+
+
+var playerOneDice1 = 'Images/dice' + randNum1 + randNum2 + randNum3 + randNum4 + randNum5 + randNum6 + '.png';
+var playerTwoDice2 = 'Images/dice' + randNum1 + randNum2 + randNum3 + randNum4 + randNum5 + randNum6 + '.png';
+var playerThreeDice3 = 'Images/dice' + randNum1 + randNum2 + randNum3 + randNum4 + randNum5 + randNum6 + '.png';
+var playerFourDice4 = 'Images/dice' + randNum1 + randNum3 + randNum3 + randNum4 + randNum5+ randNum6 + '.png';
+var playerFiveDice5 = 'Images/dice' + randNum1 + randNum2 + randNum3 + randNum4 + randNum5 + randNum6 +'.png';
+var playerSixDice6 = 'Images/dice' + randNum1 + randNum2 + randNum3 + randNum4 + randNum5 + randNum6 + '.png';
+var playerSevenDice7 = 'Images/dice' + randNum1 + randNum2 + randNum3 + randNum4 + randNum5 + randNum6 + '.png';
+var playerEightDice8 = 'Images/dice' + randNum1 + randNum2 + randNum3 + randNum4 + randNum5 + randNum6 + '.png';
+var playerNineDice9 = 'Images/dice' + randNum1 + randNum2 + randNum3 + randNum4 + randNum5 + randNum6 + '.png';
+var playerTenDice10 = 'Images/dice' + randNum1 + randNum2 + randNum3 + randNum4 + randNum5 + randNum6 + '.png';
+
+document.querySelector('.img1').setAttribute('src',playerOneDice1)
+document.querySelector('.img2').setAttribute('src',playerTwoDice2)
+document.querySelector('.img3').setAttribute('src',playerThreeDice3)
+document.querySelector('.img4').setAttribute('src',playerFourDice4)
+document.querySelector('.img5').setAttribute('src',playerFiveDice5)
+document.querySelector('.img6').setAttribute('src',playerSixDice6)
+document.querySelector('.img7').setAttribute('src',playerSevenDice7)
+document.querySelector('.img8').setAttribute('src',playerEightDice8)
+document.querySelector('.img9').setAttribute('src',playerNineDice9)
+document.querySelector('.img10').setAttribute('src',playerTenDice10)
 
 
 
 
 
-
-let playerCount = 10;
-
-
-document.querySelector('.img1').setAttribute('src',playerOneDice1);
-document.querySelector('.img2').setAttribute('src',playerTwoDice2);
-document.querySelector('.img3').setAttribute('src',playerTwoDice3)
-document.querySelector('.img4').setAttribute('src',playerTwoDice4)
-document.querySelector('.img5').setAttribute('src',playerTwoDice5)
-document.querySelector('.img6').setAttribute('src',playerTwoDice6)
-document.querySelector('.img7').setAttribute('src',playerTwoDice7)
-document.querySelector('.img8').setAttribute('src',playerTwoDice8)
-document.querySelector('.img9').setAttribute('src',playerTwoDice9)
-document.querySelector('.img10').setAttribute('src',playerTwoDice10)
-
-function playerRound(){
-    var randNum1 = Math.floor((Math.random() * 6) + 1);
-    var playerOneDice1 = 'Images/dice' + randNum1 + '.png';
-    var randNum2= Math.floor((Math.random() * 6) + 1);
-    var playerTwoDice2 = 'Images/dice' + randNum2 + '.png';
-    var randNum3= Math.floor((Math.random() * 6) + 1);
-    var playerThreeDice3 = 'Images/dice' + randNum3 + '.png';
-    var randNum4= Math.floor((Math.random() * 6) + 1);
-    var playerFourDice4 = 'Images/dice' + randNum4 + '.png';
-    var randNum5= Math.floor((Math.random() * 6) + 1);
-    var playerFiveDice5 = 'Images/dice' + randNum5 + '.png';
-    var randNum6= Math.floor((Math.random() * 6) + 1);
-    var playerSixDice6 = 'Images/dice' + randNum6 + '.png';
-    var randNum7= Math.floor((Math.random() * 6) + 1);
-    var playerSevenDice7 = 'Images/dice' + randNum7 + '.png';
-    var randNum8= Math.floor((Math.random() * 6) + 1);
-    var playerEightDice8 = 'Images/dice' + randNum8 + '.png';
-    var randNum9= Math.floor((Math.random() * 6) + 1);
-    var playerNineDice9 = 'Images/dice' + randNum9 + '.png';
-    var randNum10= Math.floor((Math.random() * 6) + 1);
-    var playerTenDice10 = 'Images/dice' + randNum10 + '.png';
-
-    playerRoundSum = playerOneDice1 + plaey
-}
-
-function firstRound(){
-    
-    
-    for()
-    
-}
 
 
 
